8dcd7ba88453e2d3c9749bcd40aa74809d7ee905
raydim <snagin2004@gmail.com>
2020-09-28 22:38:26 +0300

    layout users ready

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import style from './users.module.css';
+import './users.css';
 import userPhoto from '../img/user.png';
-import {NavLink} from "react-router-dom";
+import {NavLink, useHistory} from "react-router-dom";
 import Preloader from "../common/preloader/preloader";
 import {Paginator} from "./paginator";
+import scroll from "./../img/scroll.svg";
+
 
 const Users = (props) => {
+    const history = useHistory();
     if (!props.users) {
         return <Preloader />
     }
+    if (history.location.pathname === "/users") {
+        window.addEventListener('scroll', function() {
+            var stop = (document.body.scrollTop || document.documentElement.scrollTop);
+            if (stop > 800) {
+                document.getElementById("scrollToTop").style.opacity = "1";
+            } else {
+                document.getElementById("scrollToTop").style.opacity = "0";
+            }
+            document.getElementById("scrollToTop").onclick = function() {
+                window.scrollTo(0,0);
+            };
+        });
+    }
+    
 
     return (
-    <div className={style.friends}>
+    <div className={"friends"}>
         <div>
-            <h1>Layout Is Not Ready</h1>
-            <div className= {style.paginator}>
+            <div className= {"paginator"}>
             <Paginator pageSize={props.pageSize}
                     totalItemsCount={props.totalUserCount}
                     currentPage={props.currentPage}
@@ -22,35 +38,38 @@ const Users = (props) => {
                     portionSize={10} />
             </div>
             {
-                props.users.map(u => <div className={style.text} key={u.id}>
-                    <span>
-                        <div>
-                            <NavLink to = {'/profile/' + u.id}>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={style.photo}/>
-                            </NavLink>
+                props.users.map(u => <div className={"text"} key={u.id}>
+                    <div>
+                        <div className={"top"}>
+                            <div>
+                                <NavLink to = {'/profile/' + u.id}>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={"photo"}/>
+                                </NavLink>
+                            </div>
+                            <div className={"data"}>
+                                <span>
+                                    <div>{u.name}</div>
+                                </span>
+                            </div>
                         </div>
-                        <div className={style.test}>
+                        <div className={"bottom"}>
                             {u.followed ?
-                                <button disabled={props.followingInProgress.some( id => id === u.id)} className={style.unFollowButton}
+                                <button disabled={props.followingInProgress.some( id => id === u.id)} className={"unFollowButton"}
                                         onClick={() => { props.unFollow(u.id) }}> Удалить </button>
-                                : <button disabled={props.followingInProgress.some( id => id === u.id)} className={style.followButton}
+                                : <button disabled={props.followingInProgress.some( id => id === u.id)} className={"followButton"}
                                         onClick={() => {props.follow(u.id)}}> + </button>}
-                            
-                                <button>
-                                    <NavLink to={'/dialogs/'+ u.id}>
-                                        написать
-                                    </NavLink>
-                                </button>
+                            <div className={"positionButtonMessage"}>
+                                <NavLink to={'/dialogs/'+ u.id} style={{ textDecoration: 'none' }}>
+                                    <span className={"buttonMessage"}>
+                                            написать
+                                    </span>
+                                </NavLink>
+                            </div>
                         </div>
-                    </span>
-                    <div className={style.data}>
-                        <span>
-                            <div>{u.name}</div>
-                        </span>
                     </div>
-
                 </div>)
             }
+            <div id="scrollToTop" ><img src={scroll} alt="" /></div>
         </div>
     </div>)
 }
